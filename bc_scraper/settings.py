c34d7da28ef7953a73c2cc8510eb982a88b064e3
xxristoskk <katsaros.christopher@gmail.com>
2021-03-17 04:16:54 -0400

    initializing repo

--- bc_scraper/settings.py
+++ bc_scraper/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['django_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
